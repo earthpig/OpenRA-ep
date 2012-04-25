ea7056a203faea08727c4a5aa264b8725169ffbc
earthpig <mason.christopher.thomas@gmail.com>
2012-04-25 14:31:15 -0700

    modified:   OpenRA.Mods.RA/AI/HackyAI.cs

--- OpenRA.Mods.RA/AI/HackyAI.cs
+++ OpenRA.Mods.RA/AI/HackyAI.cs
@@ -275,10 +275,11 @@ namespace OpenRA.Mods.RA.AI
 
 				activeUnits.Add(a);
 			}
-
+			
 			/* Create an attack force when we have enough units around our base. */
 			// (don't bother leaving any behind for defense.)
-			if (unitsHangingAroundTheBase.Count >= Info.SquadSize)
+			int randomizedSquadSize = random.Next(Info.SquadSize/3, Info.SquadSize*3);
+			if (unitsHangingAroundTheBase.Count >= randomizedSquadSize)
 			{
 				BotDebug("Launch an attack.");
 
@@ -314,7 +315,7 @@ namespace OpenRA.Mods.RA.AI
 						// Check how many own units we have gathered nearby...
 						var ownUnits = world.FindUnitsInCircle(a1.CenterLocation, Game.CellSize * 2)
 							.Where(unit => unit.Owner == p).ToList();
-						if (ownUnits.Count < Info.SquadSize)
+						if (ownUnits.Count < randomizedSquadSize - 3)
 						{
 							// Not enough to attack. Send more units.
 							world.IssueOrder(new Order("Stop", a1, false));
